6c46c64037718e7c9b4c69669e1a820041b073db
Kamil Niklasiński <kamil.niklasinski@gmail.com>
2020-10-27 22:40:12 +0100

    Apply GetFX abstract class
    This commit deals wiht adding a new base class: `GetFX` that later is
    extended, by specific currency exchange provider class - e.g.
    `GetFXNBP`.
    
    Main code changes (they imply some test changes too):
    - Remove _get_raw_response() method. This method was not adding much
      value, but only boilerplate code.
    - Refactor method parameters to use less default values. This should
      simplify implementation, enhance code readability and remove unecessary
      repetitions.
    - Add empty attributes in __init__(). This indicates clearly what are
      attributes (internal / protected) of the class initialization method.
    - Consolidate store response actions in single method. This commit
      separates storing response from requesting response from NBP API.
    
    Test refactoring:
    - Mock creation uses `return` statement with `request.addfinalizer()`
      instead of `yield`. The former does not work correctly - yielded
      fixture is not available for tests!
    
    Adding the abstract class:
    - Add base class: GetFX, add NBP specific: GetFxNBP
    - Refactor GetFXNBP to use constructor attributes.  Instead of calling
      _get_response() method with currency and date attributes, GetFXNBP
      object is created with currency and date parameters.
    - Replace get_today_rate() method with __str__() its method definition
      is added to GetFX base class. Could not add tests to verify it!

--- getfx.py
+++ getfx.py
@@ -6,44 +6,61 @@ NBP_API = "http://api.nbp.pl/api/exchangerates/rates/A"
 DEFAULT_CURRENCY = "CHF"
 
 
-class GetFx(object):
+class GetFX(object):
     def __init__(self):
-        self._json_resp = []
+        self._currency_code = ""
+        self._table_number = ""
+        self._effective_date = ""
+        self._rate = 0
 
-    def _get_request_url(self, currency=DEFAULT_CURRENCY, date=None):
+    def _delete(self):
+        "Garbage collection method."
+        pass
+
+    def _get_request_url(self):
+        raise NotImplementedError
+
+    def _store_response(self):
+        raise NotImplementedError
+
+    def __str__(self):
+        return "Currency\t: {}\nTable number\t: {} \
+                \nDate\t\t: {}\nFX rate\t\t: {}".format(
+                    self._currency_code, self._table_number,
+                    self._effective_date, self._rate)
+
+
+class GetFxNBP(GetFX):
+    def __init__(self, currency=DEFAULT_CURRENCY, date=None):
+        super().__init__()
+        self._get_response(currency, date)
+
+    def _delete(self):
+        print("--Teardown--")
+
+    def _get_request_url(self, currency, date):
+        "NBP API specific implementation of request URL composition."
         if date:
             url = '/'.join([NBP_API, currency, date])
         else:
             url = '/'.join([NBP_API, currency])
         return url
 
-    def _delete(self):
-        "Garbage collection method."
-        pass
+    def _store_response(self, resp):
+        "NBP API specific implementation of storing JSON response."
+        api_resp = resp.json()['rates'][0]
+        self._table_number = api_resp['no']
+        self._effective_date = api_resp['effectiveDate']
+        self._rate = api_resp['mid']
+        self._currency_code = resp.json()['code']
 
-    def _get_raw_response(self, currency=DEFAULT_CURRENCY, date=None):
-        return requests.get(self._get_request_url(currency, date))
-
-    def _store_response(self):
-        self._table_number = self._json_resp['no']
-        self._effective_date = self._json_resp['effectiveDate']
-        self._rate = self._json_resp['mid']
-
-    def _get_response(self, currency=DEFAULT_CURRENCY, date=None):
-        resp = self._get_raw_response(currency, date)
+    def _get_response(self, currency, date=None):
+        "NBP API specific implementation of requesting JSON response."
+        resp = requests.get(self._get_request_url(currency, date))
         if resp.status_code == 404:
             raise Exception("Incorrect currency code: {} or date: {}"
                             .format(currency, date))
-        self._json_resp = resp.json()['rates'][0]
-        self._currency_code = resp.json()['code']
-        self._store_response()
-
-    def get_today_rate(self, currency=DEFAULT_CURRENCY, date=None):
-        self._get_response(currency, date)
-        print("Currency\t:", self._currency_code)
-        print("Table number\t:", self._table_number)
-        print("Date\t\t:", self._effective_date)
-        print("FX rate\t\t:", self._rate)
+        self._store_response(resp)
 
 
 if __name__ == "__main__":
@@ -59,5 +76,5 @@ Please note this program comes without any warranty!""")
                         help='effective currency exchange date')
     args = parser.parse_args()
 
-    getfx = GetFx()
-    getfx.get_today_rate(currency=args.currency, date=args.date)
+    getfx = GetFxNBP(args.currency, date=args.date)
+    print(getfx)
--- getfx_test.py
+++ getfx_test.py
@@ -2,7 +2,7 @@ import requests
 from unittest.mock import patch
 
 import pytest
-from getfx import GetFx
+from getfx import GetFxNBP
 
 
 STANDARD_RESPONSE_LIST = ["CHF", "203/A/NBP/2020", "2020-10-16", 4.2571]
@@ -13,7 +13,8 @@ class ResponseGetMock(object):
     def __init__(self, code):
         self.status_code = code
 
-    def json(self):
+    @staticmethod
+    def json():
         json_response = {"table": "A",
                          "currency": "frank szwajcarski",
                          "code": "CHF",
@@ -24,11 +25,16 @@ class ResponseGetMock(object):
         return json_response
 
 
-@pytest.fixture
-def getfx():
-    getfx = GetFx()
-    yield getfx  # use generator to perform teardown
-    getfx._delete()
+@pytest.fixture(name='getfx')
+@patch.object(requests, 'get', return_value=ResponseGetMock(200))
+def fixture_getfx(mock_object, request):
+    getfx = GetFxNBP()
+    # Below is added as yield as teardown does not work
+
+    def fin():
+        getfx._delete()
+    request.addfinalizer(fin)
+    return getfx
 
 
 def test_getfx_initialization(getfx):
@@ -42,21 +48,20 @@ def test_getfx_initialization(getfx):
     ("EUR", None, "http://api.nbp.pl/api/exchangerates/rates/A/EUR"),
     ("USD", None, "http://api.nbp.pl/api/exchangerates/rates/A/USD")
  ))
-def test_URL_for_currency(getfx, currency, date, expected_url):
+def test_URL_for_date_and_currency(getfx, currency, date, expected_url):
     assert getfx._get_request_url(currency, date) == expected_url
 
 
 @patch.object(requests, 'get', return_value=ResponseGetMock(404))
-def test_mocked_URL_exception(mock_object, getfx):
+def test_mocked_URL_exception(mock_object):
     with pytest.raises(Exception):
-        getfx._get_response("R")
+        getfx = GetFxNBP("R")
+        getfx._delete()
     assert mock_object
 
 
-@patch.object(requests, 'get', return_value=ResponseGetMock(200))
-def test_mocked_json(mock_object, getfx):
-    getfx._get_response("CHF")
-    assert getfx._json_resp['mid'] == 4.2571
+def test_mocked_json(getfx):
+    assert getfx._rate == 4.2571
 
 
 @pytest.mark.parametrize("currency, date, expected_result", (
@@ -64,10 +69,10 @@ def test_mocked_json(mock_object, getfx):
     ("CHF", "2020-10-16", STANDARD_RESPONSE_LIST)
 ))
 @patch.object(requests, 'get', return_value=ResponseGetMock(200))
-def test_mocked_store_response(mock_object, getfx, currency, date,
-                               expected_result):
-    getfx._get_response(currency, date)
+def test_mocked_store_response(mock_object, currency, date, expected_result):
+    getfx = GetFxNBP(currency, date)
     assert getfx._currency_code == expected_result[0]
     assert getfx._table_number == expected_result[1]
     assert getfx._effective_date == expected_result[2]
     assert getfx._rate == expected_result[3]
+    getfx._delete()
