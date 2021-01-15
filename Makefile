387096dd4153db7c45a9d61a713e3ccefb6f54ff
Kamil Niklasiński <kamil.niklasinski@gmail.com>
2021-01-16 00:20:27 +0100

    Add setup.py and makefile rules
    Add:
    - initial version of setup.py with package definition
    - rules to make file for building package
    - ignored folders to .gitignore for building artefacts

--- Makefile
+++ Makefile
@@ -6,3 +6,18 @@ test:
 test-cov:
 	coverage run -m --source=getfx/ -m pytest
 	coverage report -m
+
+build:
+	python3 setup.py sdist bdist_wheel
+
+build-clean:
+	rm -rf dist/ get_fx_kniklas.egg-info/ build/
+
+build-test:
+	pip install -e .
+	python3 -m getfx USD
+
+requirements:
+	pip3 freeze > requirements.txt
+	git st requirements.txt
+	git diff requirements.txt
---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="get-fx-kniklas",
+    version="0.1.0",
+    author="Kamil Niklasinski",
+    author_email="kamil.niklasinski@gmail.com",
+    keywords='NBP API FX',
+    description="Get FX is tool to retrieve average FX rates from NBP",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    project_urls={
+        'source': 'https://github.com/kniklas/get-fx'
+    },
+    url="https://github.com/kniklas/get-fx",
+    packages=setuptools.find_packages(),
+    py_modules=["getfxnbp"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Financial and Insurance Industry",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.8',
+)
