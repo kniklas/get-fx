15c993d2c29049e591fb36e34cb7a49c1272e273
Kamil Niklasiński <kamil.niklasinski@gmail.com>
2021-01-24 22:29:02 +0100

    Implement src/ folder to keep source of the package
    `src` folder is introduced to improve code management of the package.
    This implied changes in:
    - unit tests: for example: path for imports, [path when pytests are
      initialised](https://stackoverflow.com/a/47188103)
    - workflow actions, including makefile (new rules)
    - package features, e.g.: entry_points
    - separate types of requirements file: test, build, dev
    - setup.py / setup.cfg changes
    - introduce tox
    - ...
    
    NOTE
    (1) Apply 'manual' package version in setup.py as it was not working
    correctly with Python 3.7 (earlier solution in setup.cfg assumed that
    getfx package is loaded), unfortunately this solution:
    [Single-sourcing the package
    version](https://packaging.python.org/guides/single-sourcing-package-version/#single-sourcing-the-package-version)
    did not work as expected.
    
    (2) It is decided to support Python from 3.7.3 version because:
    - for earlier Python versions there were issues with openssl
      dependencies which required reinstalling/manual installing of this lib
    - there were issues for using tox from source code (without build
      package) - due to lack of time it was not fixed
    - Tox for versions 3.4 and 3.5 was skipped due to issues with
    
    (3) Reaplce fixed pyenv name with dynamic in Makefile
    Before this change, Makefile used 'get-fx-venv' hardcoded name of pyenv
    environment variable. Now Make file first stores pyenv environment name
    before it sets local to multiple python versions, then after all tox
    tests are completed it gets back to original pyenv environment local.

--- Makefile
+++ Makefile
@@ -1,23 +1,45 @@
+VERSIONS = 3.7.3 3.8.5 3.9.0
+PYENV := $(shell pyenv local)
+
 test:
-	python3 getfx
-	python3 getfx USD
-	python3 getfx GBP -d 2020-11-27
+	python3 -m getfx
+	python3 -m getfx USD
+	python3 -m getfx GBP -d 2020-11-27
 
 test-cov:
-	coverage run -m --source=getfx/ -m pytest
+	coverage run -m --source=src/ -m pytest
 	coverage report -m
 
-build:
+build: clean
 	python3 -m pip install --upgrade setuptools wheel
 	python3 setup.py sdist bdist_wheel
-
-clean:
-	rm -vrf dist/ getfx/getfx.egg-info/ getfx.egg-info/ get_fx_kniklas.egg-info/ build/
-
-build-test:
 	pip install -e .
 
-requirements:
-	pip3 freeze > requirements.txt
-	git st requirements.txt
-	git diff requirements.txt
+clean:
+	rm -vrf \
+		dist/ \
+		build/ \
+		getfx/getfx.egg-info/ \
+		getfx.egg-info/ \
+		src/getfx.egg-info/ \
+		get_fx_kniklas.egg-info/
+
+install-dependencies:
+	pip install -r requirements-build.txt
+	pip install -r requirements-test.txt
+	pip install -r requirements-dev.txt
+
+remove-dependencies:
+	pip freeze | grep -v "^-e" | xargs pip uninstall -y
+
+tox: python
+	pyenv local $(VERSIONS)
+	pip install tox tox-pyenv
+	pip install -U pip
+	tox
+	pyenv local $(PYENV)
+
+python:
+	for i in $(VERSIONS); do \
+		pyenv install -s $i; \
+	done
--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 import setuptools
+from glob import glob
+from os.path import basename
+from os.path import splitext
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -6,23 +9,25 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="getfx",
-    version="0.1.0",
     author="Kamil Niklasinski",
     license="Apache 2",
     author_email="kamil.niklasinski@gmail.com",
+    version="0.1.0",
     keywords='NBP API FX',
     description="Get FX is tool to retrieve average FX rates from NBP",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    package_dir={'': 'getfx'},
-    packages=setuptools.find_packages(where='getfx'),
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src'),
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
     install_requires=['requests'],
-    setup_requires=['wheel'],
+    setup_requires=['setuptools', 'wheel'],
+    entry_points={
+        "console_scripts": ["getfx = getfx.getfxnbp:init_cmd"]},
     project_urls={
         'source': 'https://github.com/kniklas/get-fx'
     },
     url="https://github.com/kniklas/get-fx",
-    #  py_modules=["getfxnbp"],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Console",
@@ -32,5 +37,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.7',
+    platforms='any',
 )
