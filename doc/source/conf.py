bb464a7fdd1ba5a47dedcea82a55e0dbea791009
Kamil Niklasiński <kamil.niklasinski@gmail.com>
2021-02-26 23:55:26 +0100

    Add flake8 ignore for E402 in conf.py
    This ignore is required to make sure that getfx package is loaded to
    read __version__ from the package.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -3,7 +3,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath('../../src'))
 
-import getfx
+import getfx  # noqa: E402
 
 
 # -- Path setup --------------------------------------------------------------
